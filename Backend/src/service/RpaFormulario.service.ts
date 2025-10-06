edd7f698af09be4d4565543a480c49a9f13b2cd7
arandasalvaj <arandasalvaj@gmail.com>
2025-10-06 19:15:12 -0300

    Servicio Actualizado a RPA_Formulario

--- Backend/src/service/RpaFormulario.service.ts
+++ Backend/src/service/RpaFormulario.service.ts
@@ -1,7 +1,5 @@
-import { FindOptions, Op } from 'sequelize';
-// src/services/rpa-formulario.service.ts
 import { QueryTypes } from 'sequelize';
-import { sequelize } from '../db/sequelize'; // ajusta la ruta a tu instancia
+import { sequelize } from '../config/databaseEnti'
 
 export interface InformeUrgenciaRow {
   formulario: string;
@@ -40,48 +38,18 @@ function atStartOfDay(d: Date) {
 
 function atEndOfDay(d: Date) {
   const x = new Date(d);
-  // Para compatibilidad con SQL Server DATETIME, usar ~.997ms
   x.setHours(23, 59, 59, 997);
   return x;
 }
 
 export class RpaFormularioService {
-  /**
-   * Llama al SP REPORTERIA_GetInformeUrgencia en SQL Server.
-   * Ejemplo SQL: EXEC [BD_ENTI_CORPORATIVA].dbo.REPORTERIA_GetInformeUrgencia @FechaInicio, @FechaFin, @tipoFormu, @box
-   */
-  async getInformeUrgencia(
-    fechaInicio: Date,
-    fechaFin: Date,
-    tipoFormu: string, // p.ej. '04'
-    box: 'M' | 'U' | string // 'M' maternal, 'U' urgencias, u otro para ambos
-  ): Promise<InformeUrgenciaRow[]> {
-    // Normaliza el rango diario completo
+  async getInformeUrgencia(fechaInicio: Date,fechaFin: Date,tipoFormu: string,box: 'M' | 'U' | string ): Promise<InformeUrgenciaRow[]> {
+
     const fIni = atStartOfDay(fechaInicio);
     const fFin = atEndOfDay(fechaFin);
-
-    // IMPORTANTE: si tu conexión NO apunta por defecto a BD_ENTI_CORPORATIVA,
-    // prefija siempre con [BD_ENTI_CORPORATIVA].dbo
-    const sql = `
-      EXEC [BD_ENTI_CORPORATIVA].dbo.REPORTERIA_GetInformeUrgencia
-        @FechaInicio = :fechaInicio,
-        @FechaFin    = :fechaFin,
-        @tipoFormu   = :tipoFormu,
-        @box         = :box
-    `;
-
-    const rows = await sequelize.query<InformeUrgenciaRow>(sql, {
-      type: QueryTypes.SELECT,
-      // Sequelize (mssql) envía Date como parámetro -> SQL DATETIME/DATETIME2
-      replacements: {
-        fechaInicio: fIni,
-        fechaFin: fFin,
-        tipoFormu,
-        box,
-      },
-      // logging: console.log,
-    });
-
+    const sql = `EXEC [BD_ENTI_CORPORATIVA].dbo.REPORTERIA_GetInformeUrgencia @FechaInicio= :fechaInicio, @FechaFin= :fechaFin,@tipoFormu= :tipoFormu,@box= :box`;
+    const rows = await sequelize.query<InformeUrgenciaRow>(sql, {type: QueryTypes.SELECT,replacements:{fechaInicio: fIni ,fechaFin: fFin ,tipoFormu , box}});
     return rows;
+
   }
 }
