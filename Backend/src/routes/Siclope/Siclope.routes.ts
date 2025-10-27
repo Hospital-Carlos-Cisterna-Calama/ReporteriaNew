359dae1a7dab7b0ad60db9f3a6b47fb45b2205f8
SHuan004 <shuanca128@gmail.com>
2025-10-27 15:12:10 -0300

    27102025v4

--- Backend/src/routes/Siclope/Siclope.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { SiclopeController } from '../../controller/Siclope.controller';
-import { asyncHandler } from '../../utils';
-
-const siclopeRouter = Router();
-
-siclopeRouter.get('/nomina', asyncHandler(SiclopeController.reporteNominaPorFecha));
-
-export default siclopeRouter;
